9b7e378ec5c19f7cb56283b03932f4a12130c621
admin <admin@admins-MacBook-Pro.local>
2023-07-11 13:21:37 +0100

    add route table

--- main.tf
+++ main.tf
@@ -23,3 +23,26 @@ resource "aws_subnet" "public" {
     Name = "public"
   }
 }
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.simonvpc.id
+  cidr_block = "192.168.0.16/28"
+
+  tags = {
+    Name = "private"
+  }
+}
+
+
+resource "aws_route_table" "simonrt" {
+  vpc_id = aws_vpc.simonvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.simonigw.id
+  }
+
+    tags = {
+    Name = "simonrt"
+  }
+}
